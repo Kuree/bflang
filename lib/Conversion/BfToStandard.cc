2d7467e682a77da77737f8a00097f3297f42993d
Keyi Zhang <keyi@cs.stanford.edu>
2024-12-26 20:14:34 -0800

    [conversion] add ptr arith conversion

--- lib/Conversion/BfToStandard.cc
+++ lib/Conversion/BfToStandard.cc
@@ -46,10 +46,17 @@ void insertGlobalSymbols(mlir::Operation *rootOp) {
     // functions
     auto getCharTy = builder.getFunctionType({}, {i8});
     auto putCharTy = builder.getFunctionType({i8}, {});
-    if (!symbolTable.lookup(kGetCharName))
-        builder.create<mlir::func::FuncOp>(loc, kGetCharName, getCharTy);
-    if (!symbolTable.lookup(kPutCharName))
-        builder.create<mlir::func::FuncOp>(loc, kPutCharName, putCharTy);
+    if (!symbolTable.lookup(kGetCharName)) {
+        auto funcOp =
+            builder.create<mlir::func::FuncOp>(loc, kGetCharName, getCharTy);
+        funcOp.setVisibility(mlir::SymbolTable::Visibility::Private);
+    }
+
+    if (!symbolTable.lookup(kPutCharName)) {
+        auto funcOp =
+            builder.create<mlir::func::FuncOp>(loc, kPutCharName, putCharTy);
+        funcOp.setVisibility(mlir::SymbolTable::Visibility::Private);
+    }
 }
 
 struct ConvertPtrIncrement : mlir::OpConversionPattern<mlir::bf::IncrementPtr> {
@@ -75,6 +82,95 @@ struct ConvertPtrIncrement : mlir::OpConversionPattern<mlir::bf::IncrementPtr> {
     }
 };
 
+struct ConvertPtrDecrement : mlir::OpConversionPattern<mlir::bf::DecrementPtr> {
+    using mlir::OpConversionPattern<
+        mlir::bf::DecrementPtr>::OpConversionPattern;
+
+    mlir::LogicalResult
+    matchAndRewrite(mlir::bf::DecrementPtr op, OpAdaptor adaptor,
+                    mlir::ConversionPatternRewriter &rewriter) const override {
+        rewriter.setInsertionPoint(op);
+        auto loc = rewriter.getUnknownLoc();
+        auto ptrTy = rewriter.getType<mlir::LLVM::LLVMPointerType>();
+        auto i32 = rewriter.getI32Type();
+        auto ptr = rewriter.create<mlir::LLVM::AddressOfOp>(loc, ptrTy,
+                                                            kDataPointerName);
+        auto load = rewriter.create<mlir::LLVM::LoadOp>(loc, i32, ptr);
+        // add -1
+        auto negOne = rewriter.create<mlir::arith::ConstantIntOp>(loc, -1, 32);
+        auto add =
+            rewriter.create<mlir::arith::AddIOp>(op.getLoc(), load, negOne);
+        rewriter.create<mlir::LLVM::StoreOp>(loc, add, ptr);
+        rewriter.eraseOp(op);
+        return mlir::success();
+    }
+};
+
+struct ConvertDataIncrement
+    : mlir::OpConversionPattern<mlir::bf::IncrementData> {
+    using mlir::OpConversionPattern<
+        mlir::bf::IncrementData>::OpConversionPattern;
+
+    mlir::LogicalResult
+    matchAndRewrite(mlir::bf::IncrementData op, OpAdaptor adaptor,
+                    mlir::ConversionPatternRewriter &rewriter) const override {
+        rewriter.setInsertionPoint(op);
+        auto loc = rewriter.getUnknownLoc();
+        auto ptrTy = rewriter.getType<mlir::LLVM::LLVMPointerType>();
+        auto i32 = rewriter.getI32Type();
+        auto i8 = rewriter.getI8Type();
+
+        auto ptr = rewriter.create<mlir::LLVM::AddressOfOp>(loc, ptrTy,
+                                                            kDataPointerName);
+        auto ptrValue = rewriter.create<mlir::LLVM::LoadOp>(loc, i32, ptr);
+
+        auto dataPtr = rewriter.create<mlir::LLVM::AddressOfOp>(loc, ptrTy,
+                                                                kDataArrayName);
+        auto gep = rewriter.create<mlir::LLVM::GEPOp>(
+            loc, ptrTy, i8, dataPtr, llvm::SmallVector<mlir::Value>{ptrValue});
+        auto load = rewriter.create<mlir::LLVM::LoadOp>(loc, i32, gep);
+        // add 1
+        auto one = rewriter.create<mlir::arith::ConstantIntOp>(loc, 1, 32);
+        auto add = rewriter.create<mlir::arith::AddIOp>(op.getLoc(), load, one);
+        rewriter.create<mlir::LLVM::StoreOp>(loc, add, gep);
+        rewriter.eraseOp(op);
+        return mlir::success();
+    }
+};
+
+struct ConvertDataDecrement
+    : mlir::OpConversionPattern<mlir::bf::DecrementData> {
+    using mlir::OpConversionPattern<
+        mlir::bf::DecrementData>::OpConversionPattern;
+
+    mlir::LogicalResult
+    matchAndRewrite(mlir::bf::DecrementData op, OpAdaptor adaptor,
+                    mlir::ConversionPatternRewriter &rewriter) const override {
+        rewriter.setInsertionPoint(op);
+        auto loc = rewriter.getUnknownLoc();
+        auto ptrTy = rewriter.getType<mlir::LLVM::LLVMPointerType>();
+        auto i32 = rewriter.getI32Type();
+        auto i8 = rewriter.getI8Type();
+
+        auto ptr = rewriter.create<mlir::LLVM::AddressOfOp>(loc, ptrTy,
+                                                            kDataPointerName);
+        auto ptrValue = rewriter.create<mlir::LLVM::LoadOp>(loc, i32, ptr);
+
+        auto dataPtr = rewriter.create<mlir::LLVM::AddressOfOp>(loc, ptrTy,
+                                                                kDataArrayName);
+        auto gep = rewriter.create<mlir::LLVM::GEPOp>(
+            loc, ptrTy, i8, dataPtr, llvm::SmallVector<mlir::Value>{ptrValue});
+        auto load = rewriter.create<mlir::LLVM::LoadOp>(loc, i32, gep);
+        // add -1
+        auto negOne = rewriter.create<mlir::arith::ConstantIntOp>(loc, -1, 32);
+        auto add =
+            rewriter.create<mlir::arith::AddIOp>(op.getLoc(), load, negOne);
+        rewriter.create<mlir::LLVM::StoreOp>(loc, add, gep);
+        rewriter.eraseOp(op);
+        return mlir::success();
+    }
+};
+
 struct BfToStandard : impl::BFToStandardBase<BfToStandard> {
     void runOnOperation() override {
         // first, populate the global values
@@ -84,9 +180,14 @@ struct BfToStandard : impl::BFToStandardBase<BfToStandard> {
         auto context = &getContext();
 
         mlir::RewritePatternSet patterns(context);
+        patterns.insert<ConvertPtrIncrement, ConvertPtrDecrement,
+                        ConvertDataIncrement, ConvertDataDecrement>(context);
 
         mlir::ConversionTarget target(*context);
         target.addIllegalDialect<mlir::bf::BFDialect>();
+        target.addLegalDialect<mlir::arith::ArithDialect>();
+        target.addLegalDialect<mlir::cf::ControlFlowDialect>();
+        target.addLegalDialect<mlir::LLVM::LLVMDialect>();
         if (mlir::failed(mlir::applyPartialConversion(getOperation(), target,
                                                       std::move(patterns))))
             return signalPassFailure();
---
+++ tests/Conversion/bf-to-standard.mlir
@@ -0,0 +1,62 @@
+// RUN: bf-opt --bf-to-standard --split-input-file %s | FileCheck %s
+
+func.func @ptr_increment() {
+  bf.ptr.increment
+  return
+}
+
+// CHECK-LABEL: @ptr_increment
+// CHECK: %[[PTR:.*]] = llvm.mlir.addressof @__data_ptr : !llvm.ptr
+// CHECK: %[[PTR_VAL:.*]] = llvm.load %[[PTR]] : !llvm.ptr -> i32
+// CHECK: %[[ONE:.*]] = arith.constant 1 : i32
+// CHECK: %[[ADD:.*]] = arith.addi %[[PTR_VAL]], %[[ONE]] : i32
+// CHECK: llvm.store %[[ADD]], %[[PTR]] : i32, !llvm.ptr
+
+// -----
+
+func.func @ptr_decrement() {
+  bf.ptr.decrement
+  return
+}
+
+// CHECK-LABEL: @ptr_decrement
+// CHECK: %[[PTR:.*]] = llvm.mlir.addressof @__data_ptr : !llvm.ptr
+// CHECK: %[[PTR_VAL:.*]] = llvm.load %[[PTR]] : !llvm.ptr -> i32
+// CHECK: %[[NEG_ONE:.*]] = arith.constant -1 : i32
+// CHECK: %[[ADD:.*]] = arith.addi %[[PTR_VAL]], %[[NEG_ONE]] : i32
+// CHECK: llvm.store %[[ADD]], %[[PTR]] : i32, !llvm.ptr
+
+// -----
+
+func.func @data_increment() {
+  bf.data.increment
+  return
+}
+
+// CHECK-LABEL: @data_increment
+// CHECK: %[[DATA_PTR:.*]] = llvm.mlir.addressof @__data_ptr : !llvm.ptr
+// CHECK: %[[DATA_PTR_VAL:.*]] = llvm.load %[[DATA_PTR]] : !llvm.ptr -> i32
+// CHECK: %[[DATA:.*]] = llvm.mlir.addressof @__data : !llvm.ptr
+// CHECK: %[[GEP:.*]] = llvm.getelementptr %[[DATA]][%[[DATA_PTR_VAL]]] : (!llvm.ptr, i32) -> !llvm.ptr, i8
+// CHECK: %[[VAL:.*]] = llvm.load %[[GEP]] : !llvm.ptr -> i32
+// CHECK: %[[ONE:.*]] = arith.constant 1 : i32
+// CHECK: %[[ADD:.*]] = arith.addi %[[VAL]], %[[ONE]] : i32
+// CHECK: llvm.store %[[ADD]], %[[GEP]] : i32, !llvm.ptr
+
+// -----
+
+func.func @data_decrement() {
+  bf.data.decrement
+  return
+}
+
+// CHECK-LABEL: @data_decrement
+// CHECK: %[[DATA_PTR:.*]] = llvm.mlir.addressof @__data_ptr : !llvm.ptr
+// CHECK: %[[DATA_PTR_VAL:.*]] = llvm.load %[[DATA_PTR]] : !llvm.ptr -> i32
+// CHECK: %[[DATA:.*]] = llvm.mlir.addressof @__data : !llvm.ptr
+// CHECK: %[[GEP:.*]] = llvm.getelementptr %[[DATA]][%[[DATA_PTR_VAL]]] : (!llvm.ptr, i32) -> !llvm.ptr, i8
+// CHECK: %[[VAL:.*]] = llvm.load %[[GEP]] : !llvm.ptr -> i32
+// CHECK: %[[NEG_ONE:.*]] = arith.constant -1 : i32
+// CHECK: %[[ADD:.*]] = arith.addi %[[VAL]], %[[NEG_ONE]] : i32
+// CHECK: llvm.store %[[ADD]], %[[GEP]] : i32, !llvm.ptr
+
